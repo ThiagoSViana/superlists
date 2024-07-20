4fb3b9814be833e73659e6e3455381ccbb1101d1
Thiago Viana <98830932+ThiagoSViana@users.noreply.github.com>
2024-07-19 22:00:17 -0300

    atualiza models.py

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
-class List(models.Model):
-   pass
-
 class Item(models.Model):
-   text = models.TextField(default='')
-   list = models.ForeignKey(List,on_delete=models.SET_DEFAULT,default=None)
+   text = models.TextField(
+default=''
+)
+   priority = models.TextField(
+default=''
+)
+   list = models.ForeignKey(
+       List,
+       on_delete=models.SET_DEFAULT,default=None
+)
